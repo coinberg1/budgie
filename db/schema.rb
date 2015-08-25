aeac9efcbd27c1803d9623c35785ff3fca976a23
josephweinberg <jmweinb@gmail.com>
2015-08-25 00:06:50 -0400

    fb auth

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150825030815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "homes", force: :cascade do |t|
     t.string   "Name"
     t.text     "description"
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150825030815) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
